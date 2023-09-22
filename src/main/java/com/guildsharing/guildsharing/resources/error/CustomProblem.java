d34472f746f94ddb76d8473cfbe6f55bee8f87e9
Valentin BOUFFARD <valentin.bouffard@decathlon.com>
2023-09-22 13:44:59 +0200

    fix

--- src/main/java/com/guildsharing/guildsharing/resources/error/CustomProblem.java
+++ src/main/java/com/guildsharing/guildsharing/resources/error/CustomProblem.java
@@ -1,5 +1,6 @@
 package com.guildsharing.guildsharing.resources.error;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -12,6 +13,7 @@ import java.util.List;
 
 @Data
 public class CustomProblem extends Problem {
+    @JsonIgnoreProperties({"codes", "arguments", "objectName", "rejectedValue", "bindingFailure", "code"})
     private List<FieldError> fieldErrors;
 
     public CustomProblem(URI type, String title, int status, String detail, URI instance, List<FieldError> fieldErrors) {
